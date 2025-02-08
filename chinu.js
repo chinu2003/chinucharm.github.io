c50f9caee6db903b78281c040511ee315019eaf6
Chinmay Harishchandra Mondkar <125800575+chinu2003@users.noreply.github.com>
2025-02-08 10:53:45 +0530

    cm

--- chinu.js
+++ chinu.js
@@ -1,7 +1,10 @@
+const stopBtn = document.getElementById("backBtn");
+const startBtn = document.getElementById("stopBtn");
 var i = 0;
 var txt1 =
   "Hi Love......!  <<               Now I wan't to say something special to you. <<<                So , Please read everything carefully...!                                                                           > When I saw you for the first time < You seems something Special to me.  <<                  As the days goes < you get closer to me....! <<                           I don't know the reason why your thoughts always resonates inside my mind...!                                                     > Everything about you is always intresting for me...!                     << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in future...!                                                     > I Love my Parents so much than anything else in this world....!                    << Now You are the only person  whom I love equally with my parents....!                                                             >I Love U <SweetHeart......! |                  <<<< Give me One chance to Prove my Love ...!   by untoldcoding";
 var speed = 55;
+var audio;
 typeWriter();
 function typeWriter() {
   if (i < txt1.length) {
@@ -18,8 +21,32 @@ function typeWriter() {
 }
 
 // document.querySelectorAll("#text1").addEventListener(music());
-function music(){
+var audio = new Audio("./img/love.mp3");
+function music() {
   // alert("working");
-  var audio = new Audio('./img/music.mp3');
   audio.play();
-  }
+  stopBtn.classList.add("hidden");
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+  startBtn.classList.remove("hidden");
+}
+// function stop() {
+//   if (audio) {
+//     audio.pause().catch(function(error) {
+//       console.error("Error playing audio:", error);
+//   });; // Pause the audio
+//     audio.currentTime = 0; // Reset playback position to the start
+//     stopBtn.disabled = false;
+//     startBtn.disabled = true;
+//     console.log('click');
+
+//   }
+// }
+function stop() {
+  console.log("click");
+  audio.pause();
+  stopBtn.classList.remove("hidden");
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+  startBtn.classList.add("hidden");
+}
